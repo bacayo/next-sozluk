75223d7d3201ad64edad5d85b052df1770e1163b
bacayo <batuhanyolcuu@gmail.com>
2023-07-19 16:09:30 +0300

    changed text color

--- app/components/Inputs/SearchInput.tsx
+++ app/components/Inputs/SearchInput.tsx
@@ -14,7 +14,7 @@ const SearchInput = ({ placeholder }: SearchInputProps) => {
     // <div className="relative flex items-center sm:w-4/5 lg:w-1/3 ">
     <div className="relative flex items-center w-full lg:w-1/3 ">
       <Input
-        className="transition bg-gray-200 border-2 border-green-800 outline-none focus-visible:ring-0 focus:border-green-400 placeholder:text-slate-600"
+        className="transition bg-gray-200 border-2 border-green-800 outline-none text-slate-900 focus-visible:ring-0 focus:border-green-400 placeholder:text-slate-900"
         placeholder={placeholder}
       />
       <div className="absolute flex items-center justify-center right-2 ">
