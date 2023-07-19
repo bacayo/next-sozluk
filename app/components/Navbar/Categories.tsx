2b5067d8e35650de48397abcb3c28b7fad1f1d27
bacayo <batuhanyolcuu@gmail.com>
2023-07-19 16:08:40 +0300

    added categories to navbar

---
+++ app/components/Navbar/Categories.tsx
@@ -0,0 +1,48 @@
+"use client";
+
+import React, { useState } from "react";
+
+export const categories = [
+  {
+    label: "popular",
+    description: "This property is close to the beach!",
+  },
+  {
+    label: "top",
+    description: "This property has windmills!",
+  },
+  {
+    label: "#politics",
+    description: "This property is modern!",
+  },
+  {
+    label: "#programming",
+    description: "This property is in the countryside!",
+  },
+];
+
+const Categories = () => {
+  const [categorySelected, setCategorySelected] = useState("");
+
+  return (
+    <div className="flex flex-row gap-2 py-4 md:gap-4 lg:gap-20 justify-items-start">
+      {categories.map((item) => (
+        <p
+          onClick={() => {
+            setCategorySelected(item.label);
+          }}
+          className={`px-4 border-b-8 cursor-pointer hover:border-b-8 hover:border-green-500 ${
+            categorySelected === item.label
+              ? "border-green-500"
+              : "border-neutral-800"
+          }`}
+          key={item.label}
+        >
+          {item.label}
+        </p>
+      ))}
+    </div>
+  );
+};
+
+export default Categories;
--- app/components/Navbar/Navbar.tsx
+++ app/components/Navbar/Navbar.tsx
@@ -4,10 +4,11 @@ import Container from "../Container";
 
 import SearchInput from "../Inputs/SearchInput";
 import RightContent from "./RightContent";
+import Categories from "./Categories";
 
 const Navbar = () => {
   return (
-    <div className="fixed w-full bg-neutral-800">
+    <div className="sticky top-0 z-20 w-full bg-neutral-800">
       <div className="py-4">
         <Container>
           <div className="flex flex-row items-center justify-between gap-2 md:gap-14 md:justify-items-start ">
@@ -15,6 +16,7 @@ const Navbar = () => {
             <SearchInput placeholder="topic, #entry, @author" />
             <RightContent />
           </div>
+          <Categories />
         </Container>
       </div>
     </div>
