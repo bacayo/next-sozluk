9be49d45857fcc750fa5cb4f122508ec6e303efd
bacayo <batuhanyolcuu@gmail.com>
2023-08-21 14:30:10 +0300

    fetch topic entris and randomentries

--- app/page.tsx
+++ app/page.tsx
@@ -5,27 +5,41 @@ import { cookies } from "next/headers";
 import Container from "./components/Container";
 import MainContent from "./components/MainContent";
 import Sidebar from "./components/Sidebar";
+import { IEntriesParams } from "./actions/getEntriesByTopicId";
 
-interface IParams {
-  topicId: string | null;
+interface HomeParams {
+  searchParams: IEntriesParams;
 }
 
-export default async function Home() {
+export default async function Home({ searchParams }: HomeParams) {
   const supabase = createServerComponentClient<Database>({ cookies });
 
-  let { data: topics, error } = await supabase.from("topics").select("*");
-  // const randomEntries = await getRandomEntries();
+  const { data: topics } = await supabase.from("topics").select("*");
 
   const { data: randomEntries } = await supabase
     .from("random_entries")
     .select("*,profiles(*),topics(*)");
+
+  const {
+    data: { session },
+  } = await supabase.auth.getSession();
+
+  const { data: entries } = await supabase
+    .from("topics")
+    .select(`*,entry(*,profiles(*))`)
+    .eq("id", searchParams["topic" as keyof typeof searchParams]);
 
   return (
     <Container>
       <div className="pt-28" />
       <div className="flex flex-row ">
         <Sidebar topics={topics} />
-        <MainContent randomEntries={randomEntries} />
+        <MainContent
+          randomEntries={randomEntries}
+          entries={entries}
+          searchParams={searchParams}
+          session={session}
+        />
       </div>
     </Container>
   );
