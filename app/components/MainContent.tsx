169cd4f0ce75007a497f62a50a4037cba6c9e647
bacayo <batuhanyolcuu@gmail.com>
2023-08-21 14:22:52 +0300

    dynamic changes main content

--- app/components/MainContent.tsx
+++ app/components/MainContent.tsx
@@ -1,85 +1,50 @@
 "use client";
 
-import { useEffect, useState } from "react";
-import { getEntriesByTopicId } from "../actions/getEntriesByTopicId";
+import { Session } from "@supabase/supabase-js";
+import { IEntriesParams } from "../actions/getEntriesByTopicId";
 import { useAppSelector } from "../hooks/reduxHooks";
+import EntryForm from "./EntryForm";
 import Entry from "./Topic/Entry";
 import Topic from "./Topic/Topic";
 import TopicBox from "./Topic/TopicBox";
-import { createClientComponentClient } from "@supabase/auth-helpers-nextjs";
-import { Database } from "@/lib/supabase";
-import { useRouter } from "next/navigation";
-
-type Entry =
-  | {
-      created_at: string;
-      id: string;
-      title: string;
-      updated_at: string | null;
-      user_id: string;
-      entry: {
-        created_at: string;
-        id: string;
-        text: string;
-        topic_id: string;
-        updated_at: string | null;
-        user_id: string;
-        profiles: {
-          avatar_url: string | null;
-          id: string;
-          updated_at: string | null;
-          username: string | null;
-        } | null;
-      }[];
-    }[]
-  | null;
-
-type RandomEntries =
-  | {
-      created_at: string | null;
-      id: string | null;
-      text: string | null;
-      topic_id: string | null;
-      updated_at: string | null;
-      user_id: string | null;
-      profiles: {
-        avatar_url: string | null;
-        id: string;
-        updated_at: string | null;
-        username: string | null;
-      } | null;
-      topics: {
-        created_at: string;
-        id: string;
-        title: string;
-        updated_at: string | null;
-        user_id: string;
-      } | null;
-    }[]
-  | null;
+import { RandomEntries, EntryT } from "@/types/types";
 
 interface MainContentProps {
   randomEntries?: RandomEntries;
+  entries?: EntryT;
+  searchParams?: IEntriesParams;
+  session: Session | null;
 }
 
-const MainContent = ({ randomEntries }: MainContentProps) => {
+const MainContent = ({
+  randomEntries,
+  entries,
+  searchParams,
+  session,
+}: MainContentProps) => {
   const { topicId } = useAppSelector((state) => state.setTopicId);
-  const [entry, setEntry] = useState<Entry>();
+  const { searchQueryString } = useAppSelector((state) => state.searchQuery);
 
-  // const supabase = createClientComponentClient<Database>();
-  const router = useRouter();
-  useEffect(() => {
-    const getEntries = async () => {
-      const entries = await getEntriesByTopicId(topicId as string);
-      setEntry(entries);
-    };
-    if (topicId) {
-      getEntries();
-      router.refresh();
-    }
-  }, [topicId, router]);
+  if (searchParams!["topic" as keyof typeof searchParams] === topicId)
+    return (
+      <div className="flex-grow text-gray-200">
+        <div className="flex flex-col gap-4">
+          <div>
+            {entries?.map((entry) => (
+              <div key={entry.id}>
+                <Topic topic={entry.title} />
+                {entry.entry.map((item) => (
+                  <Entry key={item.id} entry={item} />
+                ))}
+              </div>
+            ))}
+          </div>
+          {session && <EntryForm />}
+        </div>
+      </div>
+    );
 
-  if (!topicId) {
+  if (searchParams!["topic" as keyof typeof searchParams] === undefined) {
     return (
       <TopicBox>
         {randomEntries?.map((item) => (
@@ -93,13 +58,19 @@ const MainContent = ({ randomEntries }: MainContentProps) => {
   }
 
   return (
-    <div className="flex-grow text-gray-200">
-      {entry?.map((item) => (
-        <Topic key={item.id} topic={item.title.toLowerCase()} />
-      ))}
-      {entry?.map((item) =>
-        item.entry.map((i) => <Entry key={i.id} entry={i} />)
-      )}
+    <div className="flex flex-col flex-grow gap-2 ml-2">
+      <h1 className="text-2xl font-bold text-green-600 hover:underline hover:cursor-pointer">
+        {searchQueryString}
+      </h1>
+      <div className="text-sm text-gray-200">
+        <p>There is no such thing in this universe, yet</p>
+        <p>Someone better populate this</p>
+      </div>
+      {/* Text area */}
+      <div>
+        {/* <Textarea /> */}
+        {session && <EntryForm />}
+      </div>
     </div>
   );
 };
