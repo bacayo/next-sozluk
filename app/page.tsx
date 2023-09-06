8092279881207ed6f3976010032db57112ff4df5
unknown <batuhanyolcuu@gmail.com>
2023-09-06 14:43:41 +0300

    added session prop to maincontent in order to add new entry if logged

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,8 @@ import { cookies } from "next/headers";
 import Container from "./components/Container";
 import MainContent from "./components/MainContent";
 import Sidebar from "./components/Sidebar";
-import { IEntriesParams } from "./actions/getEntriesByTopicId";
+import Topic from "./components/Topic/Topic";
+import Entry from "./components/Topic/Entry";
 
 export default async function Home() {
   const supabase = createServerComponentClient<Database>({ cookies });
@@ -14,18 +15,18 @@ export default async function Home() {
 
   const { data: randomEntries } = await supabase
     .from("random_entries")
-    .select("*,profiles(*),topics(*)");
+    .select("*,favorites(*),topics(*),profiles(*)");
 
-  // const {
-  //   data: { session },
-  // } = await supabase.auth.getSession();
+  const {
+    data: { session },
+  } = await supabase.auth.getSession();
 
   return (
     <Container>
       <div className="pt-28" />
       <div className="flex flex-row ">
         <Sidebar topics={topics} />
-        <MainContent randomEntries={randomEntries} />
+        <MainContent randomEntries={randomEntries} session={session} />
       </div>
     </Container>
   );
