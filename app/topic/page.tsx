52b560598dbe261a80558cb0907ab99220ae2bb3
unknown <batuhanyolcuu@gmail.com>
2023-09-09 10:02:57 +0300

    fixed main content margin

--- app/topic/page.tsx
+++ app/topic/page.tsx
@@ -3,7 +3,6 @@ import { createServerComponentClient } from "@supabase/auth-helpers-nextjs";
 import { cookies } from "next/headers";
 import Container from "../components/Container";
 import EntryForm from "../components/EntryForm";
-import Sidebar from "../components/Sidebar";
 
 interface AddNewTopicPageProps {
   searchParams: {
@@ -13,19 +12,16 @@ interface AddNewTopicPageProps {
 
 const AddNewTopicPage = async ({ searchParams }: AddNewTopicPageProps) => {
   const supabase = createServerComponentClient<Database>({ cookies });
-  const { data: topics } = await supabase.from("topics").select("*");
+
   const {
     data: { session },
   } = await supabase.auth.getSession();
 
-  console.log(searchParams.q);
-
   return (
     <Container>
-      <div className="pt-28" />
-      <div className="flex flex-row ">
-        <Sidebar topics={topics} />
-        <div className="flex flex-col flex-grow gap-2 ml-2">
+      <div className="flex flex-row flex-grow w-full ">
+        {/* <Sidebar topics={topics} /> */}
+        <div className="flex flex-col flex-grow gap-2 ml-2 ">
           <h1 className="text-2xl font-bold text-green-600 hover:underline hover:cursor-pointer">
             {searchParams.q}
           </h1>
