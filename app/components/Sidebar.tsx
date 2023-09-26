e5d3e7d10383f2ce960876345b1f3d23d638206b
unknown <batuhanyolcuu@gmail.com>
2023-09-26 13:48:07 +0300

    added padding bottom  and margin top

--- app/components/Sidebar.tsx
+++ app/components/Sidebar.tsx
@@ -1,18 +1,15 @@
 "use client";
 import { Topics } from "@/types/types";
 import Link from "next/link";
-import { useRouter } from "next/navigation";
 
 interface SidebarProps {
   topics: Topics | undefined;
 }
 
 const Sidebar = ({ topics }: SidebarProps) => {
-  const router = useRouter();
-
   return (
     <>
-      <div className="fixed z-20 hidden w-64 h-full mt-4 overflow-y-auto text-white lg:block ">
+      <div className="fixed z-20 hidden w-64 h-full pb-32 overflow-y-auto text-white mt-28 lg:block">
         <div className="flex-col items-start justify-start hidden md:flex ">
           {topics?.map((item) => (
             <Link
