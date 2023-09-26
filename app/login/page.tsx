6ecdcc09f6cfc4252d0cde28af4e374e8b058a39
unknown <batuhanyolcuu@gmail.com>
2023-09-26 23:37:47 +0300

    added warning message to user if session is not null

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,11 +1,32 @@
+import { createServerComponentClient } from "@supabase/auth-helpers-nextjs";
+import { cookies } from "next/headers";
+
 import Container from "../components/Container";
+import { Database } from "@/lib/supabase";
 import LoginForm from "./LoginForm";
 
 const LoginPage = async () => {
+  const supabase = createServerComponentClient<Database>({ cookies });
+
+  const {
+    data: { session },
+  } = await supabase.auth.getSession();
+
+  const { data: user } = await supabase
+    .from("profiles")
+    .select()
+    .eq("id", session?.user.id)
+    .single();
+
   return (
     <div className="pt-28">
       <Container>
         <p className="py-2 font-bold">Login</p>
+        {session && (
+          <div className="pb-4 text-sm text-red-700">
+            {`just to let you know, you are already logged in as '${user?.username}'`}
+          </div>
+        )}
         <LoginForm />
         {/* </div> */}
       </Container>
