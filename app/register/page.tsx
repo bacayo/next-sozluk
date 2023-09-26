5819bf9e49e809f872574988aa3d624efd2900d8
unknown <batuhanyolcuu@gmail.com>
2023-09-26 23:38:20 +0300

    redirect to home page when session is not null

--- app/register/page.tsx
+++ app/register/page.tsx
@@ -1,8 +1,23 @@
 import React from "react";
+import { createServerComponentClient } from "@supabase/auth-helpers-nextjs";
+import { cookies } from "next/headers";
+import { redirect } from "next/navigation";
+
 import Container from "../components/Container";
 import RegisterForm from "./RegisterForm";
+import { Database } from "@/lib/supabase";
 
-const page = () => {
+const RegisterPage = async () => {
+  const supabase = createServerComponentClient<Database>({ cookies });
+
+  const {
+    data: { session },
+  } = await supabase.auth.getSession();
+
+  if (session) {
+    redirect("/");
+  }
+
   return (
     <div className="pt-28">
       <Container>
@@ -13,4 +28,4 @@ const page = () => {
   );
 };
 
-export default page;
+export default RegisterPage;
