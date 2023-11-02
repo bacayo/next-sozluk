32d5bd53bbb24caca7f0b1c1883238484d98fed7
unknown <batuhanyolcuu@gmail.com>
2023-11-02 12:23:52 +0300

    removed options

--- app/login/LoginForm.tsx
+++ app/login/LoginForm.tsx
@@ -85,7 +85,7 @@ const LoginForm = () => {
             const { data, error } = await supabase.auth.signInWithOAuth({
               provider: "google",
               options: {
-                redirectTo: `${location.origin}/auth/callback`,
+                // redirectTo: `${location.origin}/auth/callback`,
               },
             });
 
@@ -110,7 +110,7 @@ const LoginForm = () => {
             const { data, error } = await supabase.auth.signInWithOAuth({
               provider: "github",
               options: {
-                redirectTo: `${location.origin}/auth/callback`,
+                // redirectTo: `${location.origin}/auth/callback`,
                 // redirectTo: "http://localhost:3000/auth/callback",
               },
             });
