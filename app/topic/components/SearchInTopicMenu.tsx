3634d894d39f4972f4a951d1eab2a12093d7dc92
unknown <batuhanyolcuu@gmail.com>
2023-11-23 00:05:14 +0300

    added searchbox to topic navbar

--- app/topic/components/SearchInTopicMenu.tsx
+++ app/topic/components/SearchInTopicMenu.tsx
@@ -1,15 +1,17 @@
 "use client";
 
-import SearchInput from "@/app/components/Inputs/SearchInput";
 import { Button } from "@/app/components/ui/Button";
 import {
-  DropdownMenu,
-  DropdownMenuContent,
-  DropdownMenuGroup,
-  DropdownMenuItem,
-  DropdownMenuTrigger,
-} from "@/app/components/ui/DropdownMenu";
-import { Input } from "@/app/components/ui/Input";
+  Command,
+  CommandInput
+} from "@/app/components/ui/Command";
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from "@/app/components/ui/Popover";
+import { Database } from "@/lib/supabase";
+import { createClientComponentClient } from "@supabase/auth-helpers-nextjs";
 import { Session } from "@supabase/supabase-js";
 import {
   Calendar,
@@ -21,7 +23,7 @@ import {
 } from "lucide-react";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
-import { AiOutlineSearch } from "react-icons/ai";
+import React, { useEffect, useState } from "react";
 
 interface SearchInTopicMenuProps {
   topicTitle: string;
@@ -29,100 +31,157 @@ interface SearchInTopicMenuProps {
   session: Session | null;
 }
 
+type Profile = Database["public"]["Tables"]["profiles"]["Row"];
+
 const SearchInTopicMenu = ({
   topicTitle,
   authorName,
   session,
 }: SearchInTopicMenuProps) => {
   const pathname = usePathname();
+  const supabase = createClientComponentClient<Database>();
+  const [query, setQuery] = useState<Profile[] | null>([]);
+  const [queryText, setQueryText] = useState("");
+  const [open, setOpen] = React.useState(false);
+
+  useEffect(() => {
+    const fetchUser = async () => {
+      const { data } = await supabase
+        .from("profiles")
+        .select("*")
+        .ilike("username", `${queryText}%`);
+
+      if (queryText === "") {
+        setQuery(null);
+      } else {
+        setQuery(data);
+      }
+    };
+
+    fetchUser();
+  }, [queryText, supabase]);
 
   return (
-    <DropdownMenu>
-      <DropdownMenuTrigger asChild>
-        <Button
-          variant="outline"
-          size="icon"
-          className="font-bold text-gray-400 bg-transparent border-none w-fit h-fit hover:bg-transparent hover:text-emerald-500"
-        >
-          search in topic <ChevronDown size={18} />
-        </Button>
-      </DropdownMenuTrigger>
-      <DropdownMenuContent className="w-56 shadow shadow-neutral-700 border-neutral-800 bg-neutral-800 ">
-        <DropdownMenuGroup>
-          <Link
-            href={{
-              pathname,
-              query: {
-                a: "today",
-              },
-            }}
+    <>
+      <Popover open={open} onOpenChange={setOpen}>
+        <PopoverTrigger asChild>
+          <Button
+            variant="outline"
+            size="icon"
+            className="font-bold text-gray-400 bg-transparent border-none w-fit h-fit hover:bg-transparent hover:text-emerald-500"
           >
-            <DropdownMenuItem className="hover:cursor-pointer focus:bg-neutral-500">
-              <Calendar className="w-4 h-4 mr-2" />
-              <span>today</span>
-            </DropdownMenuItem>
-          </Link>
-          <Link
-            href={{
-              // pathname,
-              pathname: `/topic/${topicTitle}`,
-              query: {
-                a: "links",
-              },
-            }}
-          >
-            <DropdownMenuItem className="hover:cursor-pointer focus:bg-neutral-500">
-              <LinkIcon className="w-4 h-4 mr-2" />
-              <span>links</span>
-            </DropdownMenuItem>
-          </Link>
-          <DropdownMenuItem className="hover:cursor-pointer focus:bg-neutral-500">
-            <MessageSquare className="w-4 h-4 mr-2" />
-            <span>with replies</span>
-          </DropdownMenuItem>
-          <DropdownMenuItem className="hover:cursor-pointer focus:bg-neutral-500">
-            <ImageIcon className="w-4 h-4 mr-2" />
-            <span>images</span>
-          </DropdownMenuItem>
-          {session && (
-            <Link
-              href={{
-                pathname,
-                query: {
-                  a: "search",
-                  author: authorName,
-                },
-              }}
-            >
-              <DropdownMenuItem className="hover:cursor-pointer focus:bg-neutral-500">
-                <User2Icon className="w-4 h-4 mr-2" />
-                <span>my entries</span>
-              </DropdownMenuItem>
-            </Link>
-          )}
-          <DropdownMenuItem
-            asChild
-            onSelect={(e) => e.preventDefault()}
-            className="hover:cursor-pointer focus:bg-neutral-500"
-          >
-            <div className="flex items-center w-full max-w-sm ">
-              <Input
-                type="search"
-                placeholder="words or @author"
-                className="border-none bg-neutral-700 placeholder:text-gray-400 rounded-e-none ring-offset-emerald-600"
-              />
-              <Button
-                variant="default"
-                size="icon"
-                className="border-none outline-none focus:outline-none focus-visible:outline-none rounded-s-none bg-emerald-500 hover:bg-emerald-600"
-              >
-                <AiOutlineSearch className="w-4 h-4 text-gray-200" />
-              </Button>
+            search in topic <ChevronDown size={18} />
+          </Button>
+        </PopoverTrigger>
+        <PopoverContent className="w-[200px] p-0 bg-neutral-800 border-none">
+          <div className="grid gap-4 ">
+            <div className="grid gap-2 ">
+              <div className="grid items-center grid-cols-1 gap-4">
+                <Link
+                  onClick={() => setOpen(false)}
+                  className="flex items-center px-2 py-1 rounded-sm hover:cursor-pointer hover:bg-neutral-500"
+                  href={{
+                    pathname,
+                    query: {
+                      a: "today",
+                    },
+                  }}
+                >
+                  <Calendar className="w-4 h-4 mr-2" />
+                  <span>today</span>
+                </Link>
+              </div>
+              <div className="grid items-center grid-cols-1 gap-4">
+                <Link
+                  onClick={() => setOpen(false)}
+                  className="flex items-center px-2 py-1 rounded-sm hover:cursor-pointer hover:bg-neutral-500"
+                  href={{
+                    pathname: `/topic/${topicTitle}`,
+                    query: {
+                      a: "links",
+                    },
+                  }}
+                >
+                  <LinkIcon className="w-4 h-4 mr-2" />
+                  <span>links</span>
+                </Link>
+              </div>
+              <div className="grid items-center grid-cols-1 gap-4">
+                <Link
+                  onClick={() => setOpen(false)}
+                  className="flex items-center px-2 py-1 rounded-sm hover:cursor-pointer hover:bg-neutral-500"
+                  href={"/"}
+                >
+                  <MessageSquare className="w-4 h-4 mr-2" />
+                  <span>with replies</span>
+                </Link>
+              </div>
+              <div className="grid items-center grid-cols-1 gap-4">
+                <Link
+                  onClick={() => setOpen(false)}
+                  className="flex items-center px-2 py-1 rounded-sm hover:cursor-pointer hover:bg-neutral-500"
+                  href={"/"}
+                >
+                  <ImageIcon className="w-4 h-4 mr-2" />
+                  <span>images</span>
+                </Link>
+              </div>
+              {session && (
+                <div className="grid items-center grid-cols-1 gap-4">
+                  <Link
+                    onClick={() => setOpen(false)}
+                    className="flex items-center px-2 py-1 rounded-sm hover:cursor-pointer hover:bg-neutral-500"
+                    href={{
+                      pathname,
+                      query: {
+                        a: "search",
+                        author: authorName,
+                      },
+                    }}
+                  >
+                    <User2Icon className="w-4 h-4 mr-2" />
+                    <span>my entries</span>
+                  </Link>
+                </div>
+              )}
             </div>
-          </DropdownMenuItem>
-        </DropdownMenuGroup>
-      </DropdownMenuContent>
-    </DropdownMenu>
+          </div>
+          <Command className="bg-neutral-700">
+            <CommandInput
+              onValueChange={(e) => {
+                if (e.startsWith("@")) {
+                  setQueryText(e.substring(1));
+                } else {
+                  setQueryText(e);
+                }
+              }}
+              className="placeholder:text-gray-400"
+              placeholder="words or @author"
+            />
+            {query?.map((item) => (
+              <React.Fragment key={item.id}>
+                <Link
+                  href={{
+                    pathname,
+                    query: {
+                      a: "search",
+                      keywords: `@${item.username}`,
+                    },
+                  }}
+                  onClick={() => {
+                    setOpen(false);
+                    setQuery(null);
+                  }}
+                  className="px-2 py-1.5 hover:bg-neutral-500 hover:cursor-pointer text-sm text-gray-200 bg-neutral-800"
+                >
+                  @{item.username}
+                </Link>
+              </React.Fragment>
+            ))}
+          </Command>
+        </PopoverContent>
+      </Popover>
+    </>
   );
 };
 
