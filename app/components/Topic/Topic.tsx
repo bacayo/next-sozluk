18c08e1e8fd94f188393be97a48258179981f58a
unknown <batuhanyolcuu@gmail.com>
2023-11-27 14:22:23 +0300

    added param slug

--- app/components/Topic/Topic.tsx
+++ app/components/Topic/Topic.tsx
@@ -3,16 +3,19 @@
 interface TopicProps {
   topic: any;
   onClick?: () => void;
+  params: {
+    slug: string;
+  };
 }
 
-const Topic = ({ topic, onClick }: TopicProps) => {
+const Topic = ({ topic, onClick, params }: TopicProps) => {
   return (
     <>
       <h1
         onClick={onClick}
         className="p-2 text-2xl font-bold cursor-pointer text-emerald-600 hover:underline"
       >
-        {topic}
+        {topic ? topic : params.slug}
       </h1>
     </>
   );
