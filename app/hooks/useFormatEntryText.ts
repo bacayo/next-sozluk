e1f97dd0a2083ba79c0f560d15db43f29283193f
unknown <batuhanyolcuu@gmail.com>
2023-12-03 15:12:19 +0300

    fixed asteriks reference

--- app/hooks/useFormatEntryText.ts
+++ app/hooks/useFormatEntryText.ts
@@ -4,24 +4,21 @@ export const useFormatEntryText = () => {
   const formattedText = useCallback((formatText: string) => {
     const linkRegex = /\[([^\]]+) ([^\]]+)\]/g;
     const spoilerRegex = /--`spoiler`--/g;
-    const placeholderRegex = /`:(.*?)`/;
+    const placeholderRegex = /`:(.*?)`/g;
 
     const formatted = formatText
       .replace(linkRegex, (_: any, linkText: string, linkURL: string) => {
         return `<a target="blank" href="${linkText}" style="color: #10b981; cursor: pointer;">${linkURL}</a>`;
-      })
+      }) // regular expression for links. ex [https://google.com, google] -> first link, second alias
       .replace(
         spoilerRegex,
         '<span style="color: #10b981; cursor: pointer; display:flex; flex-direction:column">--spoiler--</span>'
-      )
-      //@ts-ignore
-      .replace(placeholderRegex, (text) => {
-        const extractedText = text.match(placeholderRegex);
-        if (extractedText) {
-          return `<a style="color: #10b981; cursor: pointer;" href="/topic/${extractedText[1]}">*</a>`;
-        }
-      });
-
+      ) // spoiler tag
+      .replace(
+        placeholderRegex,
+        (_, match) =>
+          `<a style="color: #10b981; cursor: pointer;" href="/topic/${match}">*</a>`
+      ); // astericks. reference to tag
     return formatted;
   }, []);
 
